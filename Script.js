3099c2d610fde4fa159e7322eb59c0f329622dba
Wesley Ribeiro <yurivolpiniribeirowesley@gmail.com>
2025-11-26 09:44:58 -0300

    Pastas

---
+++ Script.js
@@ -0,0 +1,13 @@
+function save(){
+const baseUrl = 'http://159.65.228.63/';
+
+const dadosExemplo = {
+    prioridade: 'Urgente',
+    descricao: 'Entregar trabalho final DW',
+    local: 'IFC',
+    recursosNecessarios:['Computador', 'Internet', 'Exemplos anteriores'],
+    dataLimite: '2025-12-03 00:00:00',
+    matricula: 2025306210
+}
+
+}
